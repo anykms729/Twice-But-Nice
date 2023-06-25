d6c50e31810222ffeb5b0dee635cec7081862e1a
Miso <anykms9595@gmail.com>
2023-06-25 15:11:42 +0100

    Add vuex, seperate router function to router.js page itself

--- platformfrontend/src/components/Footer.vue
+++ platformfrontend/src/components/Footer.vue
@@ -2,6 +2,7 @@
   <footer class="text-muted py-5">
     <div class="container">
       <p class="float-end mb-1">
+        <a href="#">Back to top</a>
       </p>
       <p class="mb-1">Twice but Nice by Miseong Kang |
         Contact: mieongkang@twicebutnice.com | +353 (123) 456-7890 |
