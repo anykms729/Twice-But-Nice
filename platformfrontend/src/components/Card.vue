57e9a81e6817e4106e32903191d72aed24c86a5d
Miso <anykms9595@gmail.com>
2023-06-25 13:56:59 +0100

    Add vue-router and add login function

--- platformfrontend/src/components/Card.vue
+++ platformfrontend/src/components/Card.vue
@@ -1,32 +1,26 @@
 <template>
   <div class="card shadow-sm">
     <span class="img" :style="{backgroundImage: `url(${item.imgPath})`}"/>
-
     <div class="card-body">
-      <p class="card-text">{{ item.name }}</p>
+      <p class="card-text">
+        <span>{{ item.name }} &nbsp;</span>
+        <span class="discount badge bg-danger">
+          {{ item.discountPer }}%
+        </span>
+      </p>
       <div class="d-flex justify-content-between align-items-center">
-        <button class="btn btn-primary">Purchase</button>
-        <small class="text-muted">Euro {{ lib.getNumberFormatted(item.price) }}</small>
+        <button class="btn btn-primary" @click="addToCart(item.id)">
+          <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+        </button>
+        <small class="price text-muted">
+          EUR {{ lib.getNumberFormatted(item.price) }}
+        </small>
+        <small class="real text-danger">
+          EUR {{ lib.getNumberFormatted(item.price - (item.price * item.discountPer / 100)) }}
+        </small>
       </div>
     </div>
   </div>
-
-  <!--  <div class="col">-->
-  <!--    <div class="card shadow-sm">-->
-  <!--      <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>-->
-
-  <!--      <div class="card-body">-->
-  <!--        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>-->
-  <!--        <div class="d-flex justify-content-between align-items-center">-->
-  <!--          <div class="btn-group">-->
-  <!--            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>-->
-  <!--            <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>-->
-  <!--          </div>-->
-  <!--          <small class="text-muted">9 mins</small>-->
-  <!--        </div>-->
-  <!--      </div>-->
-  <!--    </div>-->
-  <!--  </div>-->
 </template>
 
 <script>
@@ -58,5 +52,7 @@ export default {
 .card .card-body .price {
   text-decoration: line-through;
 }
+
+
 
 </style>
